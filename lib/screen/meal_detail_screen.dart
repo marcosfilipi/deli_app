17e4bbcf9f3c7cdc1bf17148ed1a9e3cb81e9af7
marcos filipi <68009723+marcosfilipi@users.noreply.github.com>
2021-09-16 16:07:14 -0300

    update screen

--- lib/screen/meal_detail_screen.dart
+++ lib/screen/meal_detail_screen.dart
@@ -1,16 +1,46 @@
+import 'package:delimeals/models/meal.dart';
 import 'package:flutter/material.dart';
+import '../dummy_data.dart';
 
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('$mealId'),
+        title: Text('${selectedMeal.title}'),
       ),
-      body: Center(
-        child: Text('DeliMeals - $mealId!'),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              selectedMeal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'Ingridients',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Container(
+            height: 200,
+            width: 300,
+            child: ListView.builder(
+              itemBuilder: (ctx, index) => Card(
+                color: Theme.of(context).accentColor,
+                child: Text(selectedMeal.ingredients[index]),
+              ),
+              itemCount: selectedMeal.ingredients.length,
+            ),
+          )
+        ],
       ),
     );
   }
